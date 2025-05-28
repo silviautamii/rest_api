a593136aded61b29c3d633073e73872c6131d8be
silviautamii <iciinptr@gmail.com>
2025-05-28 15:26:47 +0700

    Update latihan1.php

--- json/latihan1.php
+++ json/latihan1.php
@@ -1,14 +1,27 @@
 <?php 
 
-$mahasiswa = [
+// $mahasiswa =[
+//   [
 
-    "nama" => "Silvia Utami Putri",
-    "nim" => "2217020150",
-    "email" => "utamisilviaputri@gmail.com"
+// "nama" => "Silvia Utami Putri",
+// "nim" => "2217020150",
+// "email" => "utamisilviaputri@gmail.com"
 
-];
+//   ],
+//    [
+
+// "nama" => "Silvia Utami Putri",
+// "nim" => "2217020150",
+// "email" => "utamisilviaputri@gmail.com"
+//    ]
+// ];
+
+
+$dbh = new PDO('mysql:host=localhost;dbname=surat_masuk','root','');
+$db = $dbh->prepare('SELECT * FROM tb_arsip');
+$db->execute();
+$mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
 
 $data = json_encode($mahasiswa);
 echo $data;
-
 ?>
